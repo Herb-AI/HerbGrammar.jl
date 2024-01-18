ee5a4227678fe5632d0a109e407abbb7a4c15da4
Luka Janjić <luka.janjic@gmail.com>
2024-01-18 16:17:00 +0100

    add tests for probabilistic grammar

--- test/test_csg.jl
+++ test/test_csg.jl
@@ -103,6 +103,52 @@
         rm("toy_pcfg.grammar")
     end
 
+    @testset "creating probabilistic CFG" begin
+        g = @pcfgrammar begin
+            0.5 : R = |(0:2)
+            0.3 : R = x
+            0.2 : B = true | false
+        end
+        
+        @test sum(map(exp, g.log_probabilities[g.bytype[:R]])) ≈ 1.0
+        @test sum(map(exp, g.log_probabilities[g.bytype[:B]])) ≈ 1.0
+        @test g.bytype[:R] == Int[1,2,3,4]
+        @test g.bytype[:B] == Int[5,6]
+        @test :R ∈ g.types && :B ∈ g.types
+    end
+
+    @testset "creating a non-normalized PCFG" begin
+        g = @pcfgrammar begin
+            0.5 : R = |(0:2)
+            0.5 : R = x
+            0.5 : B = true | false
+        end
+        
+        @test sum(map(exp, g.log_probabilities[g.bytype[:R]])) ≈ 1.0
+        @test sum(map(exp, g.log_probabilities[g.bytype[:B]])) ≈ 1.0
+        @test g.rules == [0, 1, 2, :x, :true, :false]
+        @test g.bytype[:R] == Int[1,2,3,4]
+        @test g.bytype[:B] == Int[5,6]
+        @test :R ∈ g.types && :B ∈ g.types
+    end
+
+    @testset "Adding a rule to a probabilistic CSG" begin
+        g = @pcsgrammar begin
+            0.5 : R = x
+            0.5 : R = R + R
+        end
+
+        add_rule!(g, 0.5, :(R = 1 | 2))
+
+        @test g.rules == [:x, :(R + R), 1, 2]
+        
+        add_rule!(g, 0.5, :(B = t | f))
+        
+        @test g.bytype[:B] == Int[5, 6]
+        @test sum(map(exp, g.log_probabilities[g.bytype[:R]])) ≈ 1.0
+        @test sum(map(exp, g.log_probabilities[g.bytype[:B]])) ≈ 1.0
+    end
+    
     @testset "Test that strict equality is used during rule creation" begin
         g₁ = @csgrammar begin
             R = x
