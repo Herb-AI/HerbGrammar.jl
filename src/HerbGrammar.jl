e3b1ca205828d427af154a28eeae90a925665219
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2024-11-14 16:07:02 +0100

    Remove non-existent symbols from export

--- src/HerbGrammar.jl
+++ src/HerbGrammar.jl
@@ -18,9 +18,6 @@ include("csg/probabilistic_csg.jl")
 include("grammar_io.jl")
 
 export 
-    ContextFree, 
-    ContextSensitive,
-
     ContextSensitiveGrammar,
     AbstractRuleNode,
     RuleNode,
@@ -58,7 +55,6 @@ export
 
     SymbolTable,
     
-    change_expr,
     rulenode2expr,
     rulenode_log_probability,
 
