1c08e4e721a90a49b400b0ee136c744284f947c9
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2024-11-14 15:56:47 +0100

    Add Aqua test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,11 @@
+using Aqua
 using HerbCore
 using HerbGrammar
 using Test
 
 
 @testset "HerbGrammar.jl" verbose=true begin
+    @testset "Aqua.jl Checks" Aqua.test_all(HerbGrammar)
     include("test_csg.jl")
     include("test_rulenode_operators.jl")
     include("test_rulenode2expr.jl")
