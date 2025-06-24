0f509acc21963e7c7f8bf300b9de4bd921078944
Pamela Wochner <p.wochner@tudelft.nl>
2025-06-24 13:23:38 +0200

    Fix merge_grammars!: also update rule indices for merge_to grammar. Don't modify constraints of merge_from.

--- src/csg/csg.jl
+++ src/csg/csg.jl
@@ -169,7 +169,6 @@ function _expand_shorthand(args::Vector{Any})
     end
 end
 
-# TODO: check if domain is valid
 """
 	addconstraint!(grammar::ContextSensitiveGrammar, c::AbstractConstraint)
 
@@ -218,10 +217,17 @@ function merge_grammars!(merge_to::AbstractGrammar, merge_from::AbstractGrammar)
             mapping[i] = idx
         end
     end
-    # update constraints
-    for c in merge_from.constraints
-        addconstraint!(merge_to, c)
-        HerbCore.update_rule_indices!(c, length(merge_to.rules), mapping, merge_from.constraints)
+    # update constraints of ...
+    # ... `merge_to` grammar
+    for i in eachindex(merge_to.constraints)
+        HerbCore.update_rule_indices!(merge_to.constraints[i], length(merge_to.rules))
+    end
+
+    # ... `merge_from` grammar (mapping required)
+    from_constraints = deepcopy(merge_from.constraints) # we don't want to modify `merge_from` 
+    for i in eachindex(from_constraints)
+        HerbCore.update_rule_indices!(from_constraints[i], length(merge_to.rules), mapping, from_constraints)
+        addconstraint!(merge_to, from_constraints[i])
     end
     # Note: Tests for merging two grammars with constraints can be found in HerbConstraints.
 end
