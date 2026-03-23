67e47be1d01ff91e3365b6cd309fa0beb5e39fcb
Danila_Bren <90511996+danilBren@users.noreply.github.com>
2026-03-23 15:24:46 +0100

    Check grammar tree (#126)
    * add failing tests, stub implementations
    
    * checking_the_tree
    
    * changes required for checking constraint validity
    
    * Will not complain if children of a rule are not included in a constraint
    It could implicitely mean that we don't care what the children are.
    
    * now allowing empty children in the tree is optional.
    Default - forbidden
    
    * allow empty children whem merging grammars
    
    * test: begin testing valid constraints
    
    * copied tests from HerbConstraints
    
    * Now HerbGrammar has generic deifiniton of `is_constraint_valid` that always returns true.
    Tests are now only checking trees, not constraints.
    
    * fixed `test_deprecated`
    
    * generic implementation of `is_constraint_valid` is blank
    
    * no julia nigtly checks in CI
    
    * chore: bump patch version number
    
    ---------
    
    Co-authored-by: Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>

--- test/utils_tests.jl
+++ test/utils_tests.jl
@@ -10,6 +10,6 @@
     st = grammar2symboltable(g, DefiningAVariable)
     @test st[:x] == 1
 
-    @test_warn r"deprecated" st = SymbolTable(g, DefiningAVariable)
+    @test_deprecated st = SymbolTable(g, DefiningAVariable)
     @test st[:x] == 1
 end
